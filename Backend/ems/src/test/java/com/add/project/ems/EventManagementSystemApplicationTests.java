39204c47413b030b6a61e6f587d99ab024cb9ea6
JoeHitHard <joseph.meghanath@gmail.com>
2024-04-22 12:51:58 -0500

    EMS init

---
+++ Backend/ems/src/test/java/com/add/project/ems/EventManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.add.project.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
